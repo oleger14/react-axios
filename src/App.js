2170224268bf7a26f0b0e9212e3aa7a047643232
Oleg <oleg.tcm@gmail.com>
2024-08-09 19:01:35 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,40 +1,29 @@
 
 import React from "react";
+import { useEffect, useState } from 'react'
 import axios from "axios";
 
-const baseURL = "https://dog.ceo/api/breeds/image/random"
+
 
 function App() {
-  const [post, setPost] = React.useState(null);
 
-
-  React.useEffect(() => {
-    axios.get(`${baseURL}`).then((response) => {
-      setPost(response.data);
-    });
-  }, []);
-
-  function createPost() {
-    axios
-      .post(baseURL, {
-        title: "Hello World!",
-        body: "This is a new post."
+  const [meals, setMeals] = useState([])
+  useEffect(() => {
+    axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
+      .then((res) => {
+        setMeals(res.data.meals);
       })
-      .then((response) => {
-        setPost(response.data);
-      });
-  }
-
-  if (!post) return "No post!"
+  }, [])
 
   return (
     <div>
-      <img src="{post.random}" alt="" />
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
-      <button onClick={createPost}>Create Post</button>
+      {meals.map((meal) => (
+        <img key={meal.idMeal} src={meal.strMealThumb} alt={meal.strMeal} width={400} />
+      ))}
     </div>
-  );
+  )
 }
+
+
 
 export default App;
