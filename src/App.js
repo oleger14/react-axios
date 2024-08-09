f175e78922584873a83b3085f810d4913b21ddf5
Oleg <oleg.tcm@gmail.com>
2024-08-09 22:16:01 +0300

    a

--- src/App.js
+++ src/App.js
@@ -8,19 +8,22 @@ import axios from "axios";
 function App() {
 
   const [meals, setMeals] = useState([])
+  const [param, setParam] = useState([])
   useEffect(() => {
-    axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
+    axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=${param}')
       .then((res) => {
         setMeals(res.data.meals);
       })
-  }, [])
-
+  }, [param])
+  if (!meals) return "Неправильно ввели название блюда!"
   return (
+
     <div>
       {meals.map((meal) => (
         <img key={meal.idMeal} src={meal.strMealThumb} alt={meal.strMeal} width={400} />
       ))}
     </div>
+
   )
 }
 
